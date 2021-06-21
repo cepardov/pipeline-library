46c74b486ee591200a75fa8e0213b1951a7c6205
Cristian Pardo <cristian.pardo@externos.bci.cl>
2021-06-21 00:14:04 -0400

    Pipeline para microservicios

---
+++ vars/pipelines.groovy
@@ -0,0 +1,41 @@
+def call(String pipeline) {
+    if (pipeline == "microservicio") {
+        pipeline {
+            agent any
+            stages {
+                stage("Gradle Version") {
+                    steps {
+                        sh "./gradlew --version"
+                    }
+                }
+                stage("Cleaning workspace") {
+                    steps {
+                        sh "./gradlew clean"
+                    }
+                }
+                stage('Build') {
+                    steps {
+                        sh './gradlew build'
+                    }
+                }
+                stage('Test') {
+                    steps {
+                        sh './gradlew test'
+                    }
+                }
+            }
+
+            post {
+                always {
+                    deleteDir()
+                }
+            }
+        }
+    } else {
+        pipeline {
+            agent any
+            stages {
+            }
+        }
+    }
+}
