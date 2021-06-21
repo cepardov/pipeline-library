088b46ade9bb68f63207974fe5392ea5bedf8618
Cristian Pardo <cristian.pardo@externos.bci.cl>
2021-06-20 23:49:31 -0400

    Pipeline para microservicios

--- vars/microservicios.groovy
+++ vars/microservicios.groovy
@@ -1,37 +1,50 @@
-def call() {
-    pipeline {
-        agent any
-        stages {
-            stage("Hello") {
-                steps {
-                    sh "echo 'hello'"
+def call(String test) {
+    if (test == ms) {
+        pipeline {
+            agent any
+            stages {
+                stage("Hello") {
+                    steps {
+                        sh "echo 'hello'"
+                    }
+                }
+                stage("Gradle Version") {
+                    steps {
+                        sh "./gradlew --version"
+                    }
+                }
+                stage("Cleaning workspace") {
+                    steps {
+                        sh "./gradlew clean"
+                    }
+                }
+                stage('Build') {
+                    steps {
+                        sh './gradlew build'
+                    }
+                }
+                stage('Test') {
+                    steps {
+                        sh './gradlew test'
+                    }
                 }
             }
-            stage("Gradle Version") {
-                steps {
-                    sh "./gradlew --version"
-                }
-            }
-            stage("Cleaning workspace") {
-                steps {
-                    sh "./gradlew clean"
-                }
-            }
-            stage('Build') {
-                steps {
-                    sh './gradlew build'
-                }
-            }
-            stage('Test') {
-                steps {
-                    sh './gradlew test'
+
+            post {
+                always {
+                    deleteDir()
                 }
             }
         }
-
-        post {
-            always {
-                deleteDir()
+    } else {
+        pipeline {
+            agent any
+            stages {
+                stage("Hello") {
+                    steps {
+                        sh "echo 'hello'"
+                    }
+                }
             }
         }
     }
