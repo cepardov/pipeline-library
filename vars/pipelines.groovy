a8c536c9f2ccf2adddcb3f9cc7f024749da3f951
Cristian Pardo <cristian.pardo@externos.bci.cl>
2021-06-23 12:07:32 -0400

    pipeline

--- vars/pipelines.groovy
+++ vars/pipelines.groovy
@@ -7,34 +7,161 @@ def call(String tipo) {
                     steps {
                         sh "./gradlew --version"
                     }
+                    post{
+                        failure {
+                            script{
+                                error "Failed, exiting now..."
+                            }
+                        }
+                        aborted {
+                            script{
+                                error "aborted, exiting now..."
+                            }
+                        }
+                        unstable {
+                            script{
+                                error "Unstable, exiting now..."
+                            }
+                        }
+                    }
                 }
                 stage("Cleaning workspace") {
                     steps {
                         sh "./gradlew clean"
                     }
+                    post{
+                        failure {
+                            script{
+                                error "Failed, exiting now..."
+                            }
+                        }
+                        aborted {
+                            script{
+                                error "aborted, exiting now..."
+                            }
+                        }
+                        unstable {
+                            script{
+                                error "Unstable, exiting now..."
+                            }
+                        }
+                    }
                 }
                 stage('Build') {
                     steps {
                         sh './gradlew build'
-                    }
-                }
-                stage('Create Directory') {
-                    steps {
-                        sh 'cd deploy'
-                        sh 'cp product-dal /opt'
-                        sh 'cd ..'
-                    }
-                }
-                stage('Installing service') {
-                    steps {
-                        sh 'cp /build/libs/service.jar /opt/product-dal/service/'
-                        sh 'cd /opt/product-dal/service/'
+                        sh 'pwd'
                         sh 'ls'
+                    }
+                    post{
+                        failure {
+                            script{
+                                error "Failed, exiting now..."
+                            }
+                        }
+                        aborted {
+                            script{
+                                error "aborted, exiting now..."
+                            }
+                        }
+                        unstable {
+                            script{
+                                error "Unstable, exiting now..."
+                            }
+                        }
                     }
                 }
                 stage('Test') {
                     steps {
                         sh './gradlew test'
+                    }
+                    post{
+                        failure {
+                            script{
+                                error "Failed, exiting now..."
+                            }
+                        }
+                        aborted {
+                            script{
+                                error "aborted, exiting now..."
+                            }
+                        }
+                        unstable {
+                            script{
+                                error "Unstable, exiting now..."
+                            }
+                        }
+                    }
+                }
+                stage('Stopping Service') {
+                    steps {
+                        sh 'pwd'
+                        sh 'ls'
+                    }
+                    post{
+                        failure {
+                            script{
+                                error "Failed, exiting now..."
+                            }
+                        }
+                        aborted {
+                            script{
+                                error "aborted, exiting now..."
+                            }
+                        }
+                        unstable {
+                            script{
+                                error "Unstable, exiting now..."
+                            }
+                        }
+                    }
+                }
+                stage('Create Directory') {
+                    steps {
+                        sh 'cd deploy/'
+                        sh 'cp -r product-dal/ /opt'
+                        sh 'cd ..'
+                    }
+                    post{
+                        failure {
+                            script{
+                                error "Failed, exiting now..."
+                            }
+                        }
+                        aborted {
+                            script{
+                                error "aborted, exiting now..."
+                            }
+                        }
+                        unstable {
+                            script{
+                                error "Unstable, exiting now..."
+                            }
+                        }
+                    }
+                }
+                stage('Installing service') {
+                    steps {
+                        sh 'cp build/libs/service.jar /opt/product-dal/service/'
+                        sh 'cd /opt/product-dal/service/'
+                        sh 'ls'
+                    }
+                    post{
+                        failure {
+                            script{
+                                error "Failed, exiting now..."
+                            }
+                        }
+                        aborted {
+                            script{
+                                error "aborted, exiting now..."
+                            }
+                        }
+                        unstable {
+                            script{
+                                error "Unstable, exiting now..."
+                            }
+                        }
                     }
                 }
             }
